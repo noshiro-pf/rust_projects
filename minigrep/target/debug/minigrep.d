f56db4c1b4b84408c5e8caf65386bfb0c1eaddeb
noshiro-pf <noshiro.pf@gmail.com>
2020-12-25 15:07:22 +0900

    .

--- minigrep/target/debug/minigrep.d
+++ minigrep/target/debug/minigrep.d
@@ -1,1 +1,1 @@
-/home/noshiro/repos/rust_projects/minigrep/target/debug/minigrep: /home/noshiro/repos/rust_projects/minigrep/src/main.rs
+/home/noshiro/repos/rust_projects/minigrep/target/debug/minigrep: /home/noshiro/repos/rust_projects/minigrep/src/lib.rs /home/noshiro/repos/rust_projects/minigrep/src/main.rs
