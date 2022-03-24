f2b6d0ca0f430134fc257f41b07680c66dbddec1
noshiro-pf <noshiro.pf@gmail.com>
2022-03-24 16:21:52 +0900

    wasm-game-of-life: add create_default_universe

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -24,6 +24,26 @@ pub struct Universe {
     cells: Vec<Cell>,
 }
 
+fn create_default_universe() -> Universe {
+    let width = 64;
+    let height = 64;
+    let cells = (0..width * height)
+        .map(|i| {
+            if i % 2 == 0 || i % 7 == 0 {
+                Cell::Alive
+            } else {
+                Cell::Dead
+            }
+        })
+        .collect();
+
+    Universe {
+        width,
+        height,
+        cells,
+    }
+}
+
 #[wasm_bindgen]
 impl Universe {
     fn get_index(&self, row: u32, column: u32) -> usize {
@@ -81,24 +101,7 @@ impl Universe {
     }
 
     pub fn new() -> Universe {
-        let width = 64;
-        let height = 64;
-
-        let cells = (0..width * height)
-            .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-            })
-            .collect();
-
-        Universe {
-            width,
-            height,
-            cells,
-        }
+        create_default_universe()
     }
 
     pub fn width(&self) -> u32 {
