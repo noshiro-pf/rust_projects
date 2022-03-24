b43a97c9bcb399e47229e3c941e945a898dd7767
noshiro-pf <noshiro.pf@gmail.com>
2022-03-24 16:19:53 +0900

    wasm-game-of-life: rendering to canvas directly from memory

--- wasm-game-of-life/src/lib.rs
+++ wasm-game-of-life/src/lib.rs
@@ -1,7 +1,7 @@
 mod utils;
 
-use std::fmt;
 use wasm_bindgen::prelude::*;
+// use std::fmt;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -101,21 +101,15 @@ impl Universe {
         }
     }
 
-    pub fn render(&self) -> String {
-        self.to_string()
+    pub fn width(&self) -> u32 {
+        self.width
     }
-}
 
-impl fmt::Display for Universe {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for line in self.cells.as_slice().chunks(self.width as usize) {
-            for &cell in line {
-                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
-                write!(f, "{}", symbol)?;
-            }
-            write!(f, "\n")?;
-        }
+    pub fn height(&self) -> u32 {
+        self.height
+    }
 
-        Ok(())
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
     }
 }
